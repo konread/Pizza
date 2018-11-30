c7bb83fb05c682e622634e64baaa3d334a215ac3
Konrad Slaby <slaby.konrad@gmail.com>
2018-11-30 22:41:19 +0100

    added project context

---
+++ WebService/WebService/Context/PizzaDbContext.cs
@@ -0,0 +1,21 @@
+using System.Data.Entity;
+using WebService.Models;
+
+namespace WebService.Context
+{
+    public class PizzaDbContext : DbContext
+    {
+        public PizzaDbContext()
+            : base("name=PizzaDbContext")
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<IngredientOfOfferedPizza> IngredientsOfOfferedPizza { get; set; }
+        public DbSet<IngredientOfOrderedPizza> IngredientsOfOrderedPizza { get; set; }
+        public DbSet<OfferedPizza> OfferedPizzas { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderedPizza> OrderedPizzas { get; set; }
+    }
+}
