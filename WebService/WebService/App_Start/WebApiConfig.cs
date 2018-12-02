69d77cd7b2ed4d000f8b407d20493b4815af95e7
Konrad Slaby <slaby.konrad@gmail.com>
2018-12-02 02:13:58 +0100

    data loader controller

--- WebService/WebService/App_Start/WebApiConfig.cs
+++ WebService/WebService/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace WebService
 
             // Trasy składnika Web API
             config.MapHttpAttributeRoutes();
-
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
